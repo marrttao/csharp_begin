2611251b7eef62720c54a30f2b15401983b66f1f
marrt <marrttaoburberry@gmail.com>
2025-04-08 21:50:29 +0300

    halfly completed task 2

--- csharp_begin/Program.cs
+++ csharp_begin/Program.cs
@@ -1,3 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿namespace csharp_start
+{
+    public class Program
+    {
+        public int Percentage(int total, int percent)
+        {
+            return (total * percent) / 100;
+        }
 
-Console.WriteLine("Hello, World!");
+        public int[] getTwoNumbers()
+        {
+            int a, b;
+            Console.WriteLine("Enter First Number");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Second Number");
+            b = int.Parse(Console.ReadLine());
+            int[] arr = { a, b };
+            return arr;
+        }
+
+        public static void Main(string[] args)
+        {
+            Program p = new Program();
+            int[] arr = p.getTwoNumbers();
+            Console.WriteLine("The percentage " + arr[1] + " from " + arr[0] + " is " + p.Percentage(arr[0], arr[1]));
+        }
+    }
+}
--- csharp_begin/obj/Debug/net9.0/csharp_begin.AssemblyInfo.cs
+++ csharp_begin/obj/Debug/net9.0/csharp_begin.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("csharp_begin")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46794155a523a96b97f3f2576b3924bcc9523a8c")]
 [assembly: System.Reflection.AssemblyProductAttribute("csharp_begin")]
 [assembly: System.Reflection.AssemblyTitleAttribute("csharp_begin")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
