82354337a19a3fbb086ebc774bd14c912a700853
marrt <marrttaoburberry@gmail.com>
2025-04-08 16:47:29 +0300

    Completed task 1

--- csharp_begin/Program.cs
+++ csharp_begin/Program.cs
@@ -1,3 +1,52 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿namespace csharp_start
+{
+    public class Program
+    {
+        public int GetUserInput()
+        {
+            int user;
+            // readline user
+            Console.WriteLine("Write a number between 1 and 100");
+            user = int.Parse(Console.ReadLine());
+            while (true)
+            {
+                if (user < 1 || user > 100)
+                {
+                    Console.WriteLine("Write a number between 1 and 100");
+                    user = int.Parse(Console.ReadLine());
+                }
+                else
+                {
+                    return user;
+                }
+            }
+        }
 
-Console.WriteLine("Hello, World!");
+        public void CheckNumber(int user)
+        {
+            if (user % 3 == 0 && user % 5 == 0)
+            {
+                Console.WriteLine("FizzBuzz");
+            }
+            else if (user % 3 == 0)
+            {
+                Console.WriteLine("Fizz");
+            }
+            else if (user % 5 == 0)
+            {
+                Console.WriteLine("Buzz");
+            }
+            else
+            {
+                Console.WriteLine(user);
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            Program p = new Program();
+            int user = p.GetUserInput();
+            p.CheckNumber(user);
+        }
+    }
+}
