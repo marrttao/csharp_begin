fae4b17533b1c9f6e288e261ae55b50a7b8d3b7c
marrt <marrttaoburberry@gmail.com>
2025-04-09 18:12:08 +0300

    !!!

--- csharp_begin/obj/Debug/net9.0/csharp_begin.AssemblyInfo.cs
+++ csharp_begin/obj/Debug/net9.0/csharp_begin.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("csharp_begin")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82354337a19a3fbb086ebc774bd14c912a700853")]
 [assembly: System.Reflection.AssemblyProductAttribute("csharp_begin")]
 [assembly: System.Reflection.AssemblyTitleAttribute("csharp_begin")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
