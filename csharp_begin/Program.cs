789478fe18591d089acdf90f957828eec8c9a548
marrt <marrttaoburberry@gmail.com>
2025-04-09 20:13:05 +0300

    completed task 3

--- csharp_begin/Program.cs
+++ csharp_begin/Program.cs
@@ -1,3 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿namespace csharp_begin
+{
+    public class Program
+    {
+        // Сделаем метод public для тестов
+        public static int connect_inputs(string[] arr)
+        {
+            string result = string.Empty;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                result += arr[i];
+            }
+            int result_int = int.Parse(result);
+            return result_int;
+        }
 
-Console.WriteLine("Hello, World!");
+        static string[] get_inputs()
+        {
+            string[] arr = new string[4];
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine("Enter a number: ");
+                arr[i] = Console.ReadLine();
+            }
+            return arr;
+        }
+
+        public static void Main(string[] args)
+        {
+            string[] input = get_inputs();
+            Console.WriteLine(connect_inputs(input));
+        }
+    }
+}
--- csharp_begin/obj/Debug/net9.0/csharp_begin.AssemblyInfo.cs
+++ csharp_begin/obj/Debug/net9.0/csharp_begin.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("csharp_begin")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3163775acb148bc089316b910dc57ca3a15b4294")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bfb52ef0190c431f5776c8f2774cbd5c6007d5f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("csharp_begin")]
 [assembly: System.Reflection.AssemblyTitleAttribute("csharp_begin")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
